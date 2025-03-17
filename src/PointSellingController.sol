8bec1c9c06d17fefdc733b9a9569183196527b85
Djordje Mijovic <djordje.mijovic@gmail.com>
2025-03-17 14:32:27 +0100

    Add initial unit tests.

--- src/PointSellingController.sol
+++ src/PointSellingController.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.29;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 /// @dev Provided address for tokens iz zero
@@ -60,7 +59,7 @@ abstract contract PointSellingController is Ownable2Step {
         require(address(pToken) != address(0), ZeroAddressProvided());
         require(address(request.tokenOut) != address(0), ZeroAddressProvided());
 
-        if (!ISafe(rumpelWallet).isOwner(msg.sender)) {
+        if (rumpelWallet != msg.sender && !ISafe(rumpelWallet).isOwner(msg.sender)) {
             revert NotSafeOwner();
         }
 
@@ -101,15 +100,16 @@ abstract contract PointSellingController is Ownable2Step {
             minPrice = requests_[i].minPrice > minPrice ? requests_[i].minPrice : minPrice;
         }
 
-        uint256 tokenOutPrecision = 10 ** IERC20Metadata(address(tokenOut)).decimals();
-        uint256 amountOut = swap(pToken, tokenOut, totalPoints, totalPoints * minPrice / tokenOutPrecision);
+        uint256 amountOut = swap(pToken, tokenOut, totalPoints, minPrice);
 
         /// To be discussed if fee should be paid in point token instead
         tokenOut.transfer(msg.sender, amountOut * fee / 1e18);
 
         for (uint256 i = 0; i < wallets.length; i++) {
-            address recipient = requests[wallets[i]][pToken].recipient;
-            tokenOut.transfer(recipient, amountOut * claims[i].amountToClaim / totalPoints);
+            tokenOut.transfer(
+                requests[wallets[i]][pToken].recipient,
+                (amountOut * (1e18 - fee) / 1e18) * claims[i].amountToClaim / totalPoints
+            );
         }
     }
 
@@ -118,8 +118,8 @@ abstract contract PointSellingController is Ownable2Step {
     /// @param tokenIn address of the token to be swapped
     /// @param tokenOut address of the token to be swapped for
     /// @param amountIn amount of @param tokenIn to be swapped
-    /// @param minReturn minimal amount of @param tokenOut to be received
-    function swap(IERC20 tokenIn, IERC20 tokenOut, uint256 amountIn, uint256 minReturn)
+    /// @param minPrice minimal price in @param tokenOut precision for the swap
+    function swap(IERC20 tokenIn, IERC20 tokenOut, uint256 amountIn, uint256 minPrice)
         internal
         virtual
         returns (uint256 amountOut);
